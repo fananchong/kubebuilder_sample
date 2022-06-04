56758e6da1c040f76f9bff4061b693e70fd84125
fananchong <fananchong@126.com>
2022-06-04 13:48:38 +0800

    init example2

--- example1.sh
+++ example1.sh
@@ -4,9 +4,7 @@ set -ex
 
 mkdir -p example1
 pushd example1
-if [ ! -f go.mod ]; then
-    go mod init example1
-fi
+go mod init example1
 kubebuilder init
 kubebuilder create api --group demo --version v1 --kind Example1 --resource true --controller true --namespaced true
 popd
---
+++ example2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -ex
+
+mkdir -p example2
+pushd example2
+go mod init example2
+kubebuilder init
+kubebuilder create api --group demo --version v1 --kind Example2 --resource true --controller true --namespaced true
+popd
